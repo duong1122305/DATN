082318ddf09743ca438c3cd27ed6ce6769aa19c2
Phùng Thanh Bình <Phungthanhbinh594@gmail.com>
2024-06-04 12:27:22 +0700

    z

--- DATN.API/Controllers/UserLoginController.cs
+++ DATN.API/Controllers/UserLoginController.cs
@@ -97,7 +97,7 @@ namespace DATN.API.Controllers
             var result = await _employeeSchedule.GetScheduleFromMonthToMonth(view);
             return result;
         }
-        [HttpPut("remove")]
+        [HttpGet("remove")]
         public async Task<ResponseData<string>> RemoveEmployee(string id)
         {
             var result = await _userrepo.RemoveUser(id);
