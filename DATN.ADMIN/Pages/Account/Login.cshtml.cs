9428203eea050f913e7211b9c25794ccd4e93f13
Phùng Thanh Bình <Phungthanhbinh594@gmail.com>
2024-06-07 09:34:53 +0700

    fix search staff

--- DATN.ADMIN/Pages/Account/Login.cshtml.cs
+++ DATN.ADMIN/Pages/Account/Login.cshtml.cs
@@ -54,7 +54,7 @@ namespace DATN.ADMIN.Pages.Account
                         _contextAccessor.HttpContext.Session.SetString("Key", checkLogin.Data);
                         _contextAccessor.HttpContext.Response.Redirect(Url.Content("~/trangchu"));
                     }
-                    else if(checkLogin.Error != null)
+                    else if (checkLogin.Error != null)
                     {
                         TempData["Error"] = "Sai tài khoản hoặc mật khẩu?!";
                         Page();
