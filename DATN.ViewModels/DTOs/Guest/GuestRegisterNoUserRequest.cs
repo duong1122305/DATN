1edc4be37bdefabd83b38ee408fe5bdc3a92a65b
HUNGNV <hungwnv2001@gmail.com>
2024-06-13 22:52:45 +0700

    fix bugdata

--- DATN.ViewModels/DTOs/Guest/GuestRegisterNoUserRequest.cs
+++ DATN.ViewModels/DTOs/Guest/GuestRegisterNoUserRequest.cs
@@ -17,6 +17,7 @@ namespace DATN.ViewModels.DTOs.Guest
         [RegularExpression(@"^(09|03|07|08|05)\d{8}$", ErrorMessage = "Nhập đúng định dạng số điện thoại Việt Nam")]
         public string PhoneNumber { get; set; } // Số điện thoại
         public string? Address { get; set; }
+        public string? Email { get; set; }
        
        
     }
