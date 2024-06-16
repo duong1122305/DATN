baa9727402c2ab80639c9a0f22878b3e1d46e4d0
Nguyễn Việt <nonamesoldt@gmail.com>
2024-06-16 07:24:35 +0700

    fix: update regex voucher

--- DATN.ViewModels/DTOs/Authenticate/VoucherView.cs
+++ DATN.ViewModels/DTOs/Authenticate/VoucherView.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace DATN.ViewModels.DTOs.Authenticate
     public class VoucherView
     {
         public int? Id { get; set; }
+        [RegularExpression("^([a-zA-Z]){15,15}$",ErrorMessage ="Mã giảm giá phải đủ 15 ký tự và viết ko dấu")]
         public string VoucherCode { get; set; }
 
         public int Quantity { get; set; }
@@ -20,11 +22,9 @@ namespace DATN.ViewModels.DTOs.Authenticate
         public DateTime StartDate { get; set; }
 
         public DateTime EndDate { get; set; }
-
+        [Range(1,100,ErrorMessage ="Phải nằm trong khoảng 1-100%")]
         public decimal DiscountPercent { get; set; }
-
         public double MaxMoneyDiscount { get; set; }
-
         public double MinMoneyApplicable { get; set; }
     }
 }
