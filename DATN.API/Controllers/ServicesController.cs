ba1b2f5c6bfa916db88317ec687247bb2c6699ea
phduo <phduo@DUONGPHAM>
2024-06-19 09:21:55 +0700

    duong: api service + serviceDetail

---
+++ DATN.API/Controllers/ServicesController.cs
@@ -0,0 +1,77 @@
+﻿using DATN.Aplication.Services.IServices;
+using DATN.Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DATN.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesController : ControllerBase
+    {
+        private readonly IServiceManagementService _serviceManager;
+
+        public ServicesController(IServiceManagementService serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        [HttpGet("getAllService")]
+        public async Task<IActionResult> GetAllService()
+        {
+            var result = await _serviceManager.GetAllService();
+
+            if(result.Data == null && result.IsSuccess == false) return NotFound(result.Error);
+
+            return Ok(result);
+        }
+
+        [HttpGet("getServiceById/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            if(id == 0 || id == null) return BadRequest("Không tìm thấy sản phẩm");
+
+            var result = await _serviceManager.GetServiceById(id);
+
+            if(result.Data == null && result.IsSuccess == false) return NotFound(result.Error);
+
+            return Ok(result);
+        }
+
+        [HttpPost("createService")]
+        public async Task<IActionResult> CreateService(Service service)
+        {
+            if(!ModelState.IsValid) return BadRequest(ModelState);
+
+            var result = await _serviceManager.CreateNewService(service);
+
+            if(result.IsSuccess == false) return BadRequest(result.Error);
+
+            return Ok(result);
+        }
+
+        [HttpPut("updateService")]
+        public async Task<IActionResult> UpdateService(Service service)
+        {
+            if(!ModelState.IsValid) return BadRequest(ModelState);
+
+            var result = await _serviceManager.UpdateService(service);
+
+            if(result.IsSuccess == false) return BadRequest(result.Error);
+
+            return Ok(result);
+        }
+
+        [HttpPatch("removeService/{id}")]
+        public async Task<IActionResult> RemoveService(int id)
+        {
+            if (id == 0 || id == null) return BadRequest();
+
+            var result = await _serviceManager.RemoveService(id);
+
+            if(result.IsSuccess == false) return Content(result.Error);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ DATN.API/Controllers/ServicesDetailController.cs
@@ -0,0 +1,90 @@
+﻿using DATN.Aplication.Services;
+using DATN.Aplication.Services.IServices;
+using DATN.Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DATN.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesDetailController : ControllerBase
+    {
+        private readonly IServiceDetailManagementService _serviceDetailManagementService;
+
+        public ServicesDetailController(IServiceDetailManagementService serviceDetailManagementService)
+        {
+            _serviceDetailManagementService = serviceDetailManagementService;
+        }
+
+        [HttpGet("getAllServicesDetail")]
+        public async Task<IActionResult> GetAllServicesDetail()
+        {
+            var result = await _serviceDetailManagementService.GetAllService();
+
+            if (result.IsSuccess == false) return Content(result.Error);
+
+            return Ok(result);
+        }
+
+        [HttpGet("getServiceDetailById/{id}")]
+        public async Task<IActionResult> GetServiceDetailById(int id)
+        {
+            if (id == 0 || id == null) return BadRequest();
+
+            var result = await _serviceDetailManagementService.GetServiceById(id);
+
+            if (result.IsSuccess == false) return Content(result.Error);
+
+            return Ok(result);
+        }
+
+        [HttpPost("createServiceDetail")]
+        public async Task<IActionResult> CreateServiceDetail(ServiceDetail serviceDetail)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var result = await _serviceDetailManagementService.CreateNewService(serviceDetail);
+
+            if (result.IsSuccess == false) return Content(result.Error); 
+            
+            return Ok(result);
+        }
+
+        [HttpPut("updateServiceDetail")]
+        public async Task<IActionResult> UpdateServiceDetail(ServiceDetail serviceDetail)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            
+            var result = await _serviceDetailManagementService.UpdateService(serviceDetail);
+
+            if(result.IsSuccess == false) return Content(result.Error);
+
+            return Ok(result);
+        }
+
+        [HttpPatch("removeServiceDetail/{id}")]
+        public async Task<IActionResult> RemoveServiceDetail(int id)
+        {
+            if (id == 0 || id == null) return BadRequest();
+            
+            var result = await _serviceDetailManagementService.RemoveService(id);
+
+            if(result.IsSuccess == false) return Content(result.Error);
+
+            return Ok(result);
+        }
+
+        [HttpGet("getServiceDetailByServiceId/{id}")]
+        public async Task<IActionResult> GetServiceDetailByServiceId(int id)
+        {
+            if (id == 0 || id == null) return BadRequest();
+            
+            var result = await _serviceDetailManagementService.GetServicesByIdService(id);
+
+            if(result.IsSuccess == false) return Content(result.Error);
+
+            return Ok(result);
+        }
+    }
+}
