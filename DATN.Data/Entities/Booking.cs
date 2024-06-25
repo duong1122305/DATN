f4ad0acc9175a5672a5e5786042bb2d48892f811
HUNGNV <hungwnv2001@gmail.com>
2024-06-25 17:19:43 +0700

    add db

--- DATN.Data/Entities/Booking.cs
+++ DATN.Data/Entities/Booking.cs
@@ -16,9 +16,6 @@ namespace DATN.Data.Entities
 	{
 		public int Id { get; set; } // Khóa chính
 
-		public Guid? StaffAtCounterId { get; set; } // Khóa ngoại đến ID nhân viên thanh toán
-
-		public Guid? StaffConfirmId { get; set; } // Khóa ngoại đến ID nhân viên xác nhận đơn đặt hàng
 
 		public int? VoucherId { get; set; } // ID mã giảm giá (nếu có)
 		public double? ReducedAmount { get; set; } // số tiền giảm
@@ -31,23 +28,22 @@ namespace DATN.Data.Entities
 
 		public DateTime BookingTime { get; set; } // Thời gian đặt lịch
 
-		public DateTime? DesiredStartTime { get; set; } // Thời gian mong muốn bắt đầu
+		public DateTime? AppointmentTime { get; set; } // Thời gian mong muốn bắt đầu
 
-		public DateTime? ConfirmedTime { get; set; } // Thời gian xác nhận đơn đặt hàng
+		//public DateTime? ConfirmedTime { get; set; } // Thời gian xác nhận đơn đặt hàng
 
-		public DateTime? CustomerArrivalTime { get; set; } // Thời gian khách hàng đến
+		//public DateTime? CustomerArrivalTime { get; set; } // Thời gian khách hàng đến
 
-		public DateTime? UpdatedAt { get; set; } // Thời gian cập nhật
+		//public DateTime? UpdatedAt { get; set; } // Thời gian cập nhật
 
-		public Guid? UpdatedBy { get; set; } // Người cập nhật
-
+		//public Guid? UpdatedBy { get; set; } // Người cập nhật
+		public bool IsAddToSchedule { get; set; }
 		public BookingStatus Status { get; set; } // Trạng thái đơn hàng
 
 		public virtual ICollection<BookingDetail> BookingDetails { get; set; }
+		public virtual ICollection<HistoryAction> HistoryActions { get; set; }
 
-		public virtual User StaffConfirm { get; set; }
-		public virtual User StaffAtCounter { get; set; }
-		public virtual User StaffUpdate{ get; set; }
+	
 		public virtual Guest Guest { get; set; }
 		public virtual TypePayment TypePayment { get; set; }
 		public virtual Discount Discount { get; set; }
---
+++ DATN.Data/Entities/HistoryAction.cs
@@ -0,0 +1,16 @@
+﻿namespace DATN.Data.Entities
+{
+    public class HistoryAction
+    {
+        public int ID { get; set; }
+        public int ActionID { get; set; }
+        public int BookingID { get; set; }
+        public bool ByGuest { get; set; }
+        public Guid? ActionByID { get; set; }
+        public DateTime ActionTime { get; set; }
+        public string  Description { get; set; }
+        public virtual ActionBooking Action { get; set; }
+        public virtual Booking Booking { get; set; }
+        public virtual User ActionBy { get; set; }
+    }
+}
