844a24ebc4be1ce1078868f7834dc68d00b17714
HUNGNV <hungwnv2001@gmail.com>
2024-06-30 18:06:27 +0700

    thêm login bên khách

---
+++ DATN.Aplication/System/IAuthenticateGuest.cs
@@ -0,0 +1,17 @@
+﻿using DATN.ViewModels.Common;
+using DATN.ViewModels.DTOs.Authenticate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATN.Aplication.System
+{
+	public interface IAuthenticateGuest
+	{
+		Task<ResponseData<string>> Login(UserLoginView request);
+		Task<ResponseData<string>> Logout(string UserName);
+		
+	}
+}
