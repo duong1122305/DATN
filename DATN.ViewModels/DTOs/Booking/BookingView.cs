a34270da40e8458fb56d940332d865b27f2d3187
Nguyễn Việt <nonamesoldt@gmail.com>
2024-07-04 12:18:52 +0700

    a

--- DATN.ViewModels/DTOs/Booking/BookingView.cs
+++ DATN.ViewModels/DTOs/Booking/BookingView.cs
@@ -11,7 +11,7 @@ namespace DATN.ViewModels.DTOs.Booking
         public Guid? Id { get; set; }
         public string NameGuest { get; set; }
         public string PhoneNumber { get; set; }
-        public string Email { get; set; }
+        public string? Email { get; set; }
         public string Address { get; set; }
         public DateTime BookingTime { get; set; }
     }
