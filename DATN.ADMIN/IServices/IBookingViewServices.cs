723056aaa45afc45b3208555285793febd62a154
Phùng Thanh Bình <Phungthanhbinh594@gmail.com>
2024-07-05 00:10:04 +0700

    view detail đặt dịch vụ

--- DATN.ADMIN/IServices/IBookingViewServices.cs
+++ DATN.ADMIN/IServices/IBookingViewServices.cs
@@ -6,5 +6,6 @@ namespace DATN.ADMIN.IServices
     public interface IBookingViewServices
     {
         Task<ResponseData<List<BookingView>>> GetAll();
+        Task<ResponseData<List<ListBokingDetailInDay>>> ListBookingDetailInDay(string id, DateTime date);
     }
 }
--- DATN.ADMIN/Services/BookingViewServices.cs
+++ DATN.ADMIN/Services/BookingViewServices.cs
@@ -1,6 +1,7 @@
 ﻿using DATN.ADMIN.IServices;
 using DATN.ViewModels.Common;
 using DATN.ViewModels.DTOs.Booking;
+using System.Collections.Generic;
 
 namespace DATN.ADMIN.Services
 {
@@ -14,6 +15,12 @@ namespace DATN.ADMIN.Services
         public async Task<ResponseData<List<BookingView>>> GetAll()
         {
             return await _httpClient.GetFromJsonAsync<ResponseData<List<BookingView>>>("/api/Booking/List");
+        }
+
+        public async Task<ResponseData<List<ListBokingDetailInDay>>> ListBookingDetailInDay(string id, DateTime date)
+        {
+            var respone =  _httpClient.GetFromJsonAsync<ResponseData<List<ListBokingDetailInDay>>>($"/api/Booking/List-Booking-Detail-In-Day?id={id}&date={date}").GetAwaiter().GetResult();
+            return respone;
         }
     }
 }
