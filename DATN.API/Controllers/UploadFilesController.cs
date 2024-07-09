58aee3f11ea251222a5662c7763be4db4f64b4f1
duong pham <phduongc1tantrao2018@gmail.com>
2024-07-09 21:08:51 +0700

    duong: upload api

--- DATN.API/Controllers/UploadFilesController.cs
+++ DATN.API/Controllers/UploadFilesController.cs
@@ -4,6 +4,7 @@ using DATN.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using DATN.Data.EF;
+using DATN.API.Services;
 
 namespace DATN.API.Controllers
 {
@@ -11,49 +12,20 @@ namespace DATN.API.Controllers
     [ApiController]
     public class UploadFilesController : ControllerBase
     {
-        private readonly IOptions<CloundinarySettings> _cloundinarySettings;
-        private DATNDbContext _context;
+        private readonly IUploadFileServices _services;
 
-        public UploadFilesController(IOptions<CloundinarySettings> cloundinarySettings, DATNDbContext context)
+        public UploadFilesController(IUploadFileServices services)
         {
-            _cloundinarySettings = cloundinarySettings;
-            _context = context;
+            _services = services;
         }
 
-        [HttpPost("{userId}/files")]
-        public async Task<IActionResult> UploadFiles(int procId, List<IFormFile> files)
+        [HttpPost("uploadAvatar/{idUser}")]
+        public async Task<IActionResult> UploadAvatar(Guid idUser, IFormFile file)
         {
-            if (procId == null) return BadRequest();
+            var result = await _services.UploadAvatarAsync(idUser, file);
+            if (result.IsSuccess) return Ok(result);
 
-            var account = new Account(
-                _cloundinarySettings.Value.CloundName,
-                _cloundinarySettings.Value.ApiKey,
-                _cloundinarySettings.Value.ApiSecret
-                );
-            var cloundinary = new Cloudinary(account);
-
-            var uploadResults = new List<Dictionary<string, string>>();
-
-            foreach (var i in files)
-            {
-                var uploadParams = new ImageUploadParams()
-                {
-                    File = new FileDescription(i.FileName, i.OpenReadStream()),
-                    Folder = $"uploads/users/{procId}"
-                };
-
-                var uploadResult = await cloundinary.UploadAsync(uploadParams);
-
-                uploadResults.Add(new Dictionary<string, string>
-                {
-                    {"public_id", uploadResult.PublicId },
-                    {"url", uploadResult.SecureUrl.AbsoluteUri }
-                });
-
-                //await _context.ImageProducts.AddAsync(new ImageProduct { IdProduct = procId, UrlImage = uploadResult.SecureUrl.AbsoluteUri, IsDefault = false });
-            }
-            await _context.SaveChangesAsync();
-            return Ok(uploadResults);
+            return BadRequest(result);
         }
     }
 }
