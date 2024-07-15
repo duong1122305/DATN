613e8e4bd3695f9c74a1a009f77e73c88c98fd4e
Nguyễn Việt <nonamesoldt@gmail.com>
2024-07-16 06:04:21 +0700

    qr

--- DATN.ViewModels/DTOs/Payment/APIRequest.cs
+++ DATN.ViewModels/DTOs/Payment/APIRequest.cs
@@ -8,10 +8,10 @@ namespace DATN.ViewModels.DTOs.Payment
 {
     public class APIRequest
     {
-        public int accountNo { get; set; }
+        public long accountNo { get; set; }
         public string accountName { get; set; }
         public int acqId { get; set; }
-        public int amount { get; set; }
+        public double amount { get; set; }
         public string addInfo { get; set; }
         public string format { get; set; }
         public string template { get; set; }
--- DATN.ViewModels/DTOs/Payment/APIResponse.cs
+++ DATN.ViewModels/DTOs/Payment/APIResponse.cs
@@ -10,7 +10,7 @@ namespace DATN.ViewModels.DTOs.Payment
     public class APIResponse
     {
         public string code { get; set; }
-        public int desc { get; set; }
+        public string desc { get; set; }
         public DataPayment data { get; set; }
 
     }
