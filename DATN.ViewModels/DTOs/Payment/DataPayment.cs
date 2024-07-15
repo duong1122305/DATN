9e5dc9ad7156072c9376461313a534f7d73f4d82
Nguyễn Việt <nonamesoldt@gmail.com>
2024-07-16 03:29:51 +0700

    commit

---
+++ DATN.ViewModels/DTOs/Payment/DataPayment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATN.ViewModels.DTOs.Payment
+{
+    public class DataPayment
+    {
+        public int acpId { get; set; }
+        public string accountName { get; set; }
+        public string qrCode { get; set; }
+        public string qrDataURL { get; set; }
+    }
+}
