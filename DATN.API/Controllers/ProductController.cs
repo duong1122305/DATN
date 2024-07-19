73d0cafdde05f8c387c7871e6aa5d9ca645147ae
HUNGNV <hungwnv2001@gmail.com>
2024-07-20 01:58:59 +0700

    thêm sản phẩm

--- DATN.API/Controllers/ProductController.cs
+++ DATN.API/Controllers/ProductController.cs
@@ -72,6 +72,11 @@ namespace DATN.API.Controllers
         public async Task<ResponseData<List<ProductDetaiView>>> ListProductDetails()
         {
             return await _productDetaiManagementServicel.ListProductDetail();
+        }   
+        [HttpGet("list-product-details-by-id")]
+        public async Task<ResponseData<List<ProductDetaiView>>> ListProductDetailsByID (int id)
+        {
+            return await _productDetaiManagementServicel.ListProductDetailForProduct(id);
         }
     }
 }
