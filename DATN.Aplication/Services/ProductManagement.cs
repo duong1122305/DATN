4603d3abbe2f31bfdcf0e1c773a77715ad544230
Nguyễn Việt <nonamesoldt@gmail.com>
2024-07-19 23:39:41 +0700

    view product

--- DATN.Aplication/Services/ProductManagement.cs
+++ DATN.Aplication/Services/ProductManagement.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DATN.Aplication.Services
 {
-    public class ProductManagement: IProductManagement
+    public class ProductManagement : IProductManagement
     {
         IUnitOfWork _unitOfWork;
         public ProductManagement(IUnitOfWork unitOfWork)
@@ -68,6 +68,38 @@ namespace DATN.Aplication.Services
             {
                 return new ResponseData<BillProduct> { IsSuccess = false, Error = e.Message };
             }
+        }
+        public async Task<ResponseData<List<ProductSelect>>> ListProductViewSale()
+        {
+            var queryDetail = await _unitOfWork.ProductDetailRepository.GetAllAsync();
+            var query = from product in await _unitOfWork.ProductRepository.GetAllAsync()
+                        join productDetail in await _unitOfWork.ProductDetailRepository.GetAllAsync()
+                        on product.Id equals productDetail.IdProduct
+                        join brand in await _unitOfWork.BrandRepository.GetAllAsync()
+                        on product.IdBrand equals brand.Id
+                        join cate in await _unitOfWork.CategoryDetailRepository.GetAllAsync()
+                        on product.IdCategoryProduct equals cate.Id
+                        group new { product, brand, cate }
+                        by new { product, brand, cate }
+                        into view
+                        select new ProductSelect()
+                        {
+                            Name = view.Key.product.Name,
+                            Id = view.Key.product.Id,
+                            BrandName = view.Key.brand.Name,
+                            CateName = view.Key.cate.Name,
+                            ListProductDetail = (from tetsa in queryDetail
+                                                 where tetsa.IdProduct == view.Key.product.Id
+                                                 select new ProductDetailView
+                                                 {
+                                                     IdProductDetail = tetsa.Id,
+                                                     Name = tetsa.Name,
+                                                     Price = tetsa.Price,
+                                                     Quantity = tetsa.Amount
+                                                 }).ToList(),
+                        };
+
+            return new ResponseData<List<ProductSelect>>() { IsSuccess = true, Data = query.ToList() };
         }
     }
 }
