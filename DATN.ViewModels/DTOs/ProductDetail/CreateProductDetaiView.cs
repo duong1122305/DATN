3e56de18d6a8c7ec71e57b69055365b0f050e9b1
HUNGNV <hungwnv2001@gmail.com>
2024-08-01 19:20:51 +0700

    fix giao diện

--- DATN.ViewModels/DTOs/ProductDetail/CreateProductDetaiView.cs
+++ DATN.ViewModels/DTOs/ProductDetail/CreateProductDetaiView.cs
@@ -6,13 +6,13 @@ namespace DATN.ViewModels.DTOs.ProductDetail
     {
         public int Id { get; set; }
         public int IdProduct { get; set; }
-        [Required(ErrorMessage = "Không tên biến thể")]
+        [Required(ErrorMessage = "Phải có tên biến thể")]
         public string Name { get; set; }
-        [Required(ErrorMessage ="Không để trống giá")]
-        [Range(1000,double.MaxValue, ErrorMessage = "Giá trên 1000 vnđ")]
+        [Required(ErrorMessage ="Phải có giá")]
+        [Range(1000.0,100000000.0, ErrorMessage = "Giá từ 1.000vnđ- 100.000.000vnđ")]
         public double Price { get; set; }
-        [Required]
-        [Range(1,int.MaxValue)]
+        [Required( ErrorMessage = "Phải nhập số lượng")]
+        [Range(1,1000000,ErrorMessage ="Số lượng phải từ 1-100.000")]
         public int Amount { get; set; }
 
     }
