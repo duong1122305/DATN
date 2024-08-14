106472e33e622374b099e65ba1310162eb770cda
Nguyễn Việt <nonamesoldt@gmail.com>
2024-08-14 20:46:34 +0700

    push

--- DATN.ViewModels/DTOs/Brand/BrandView.cs
+++ DATN.ViewModels/DTOs/Brand/BrandView.cs
@@ -1,8 +1,11 @@
-﻿namespace DATN.ViewModels.DTOs.Brand
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DATN.ViewModels.DTOs.Brand
 {
     public class BrandView
     {
         public int? Id { get; set; }
+        [Required(ErrorMessage = "Vui lòng không để trống trường này"), RegularExpression(@"(^([a-zỳọáầảấờễàạằệếýộậốũứĩõúữịỗìềểẩớặòùồợãụủíỹắẫựỉỏừỷởóéửỵẳẹèẽổẵẻỡơôưăêâđ'A-ZỲỌÁẦẢẤỜỄÀẠẰỆẾÝỘẬỐŨỨĨÕÚỮỊỖÌỀỂẨỚẶÒÙỒỢÃỤỦÍỸẮẪỰỈỎỪỶỞÓÉỬỴẲẸÈẼỔẴẺỠƠÔƯĂÊÂĐ]{1,})((\s)([a-zỳọáầảấờễàạằệếýộậốũứĩõúữịỗìềểẩớặòùồợãụủíỹắẫựỉỏừỷởóéửỵẳẹèẽổẵẻỡơôưăêâđ'A-ZỲỌÁẦẢẤỜỄÀẠẰỆẾÝỘẬỐŨỨĨÕÚỮỊỖÌỀỂẨỚẶÒÙỒỢÃỤỦÍỸẮẪỰỈỎỪỶỞÓÉỬỴẲẸÈẼỔẴẺỠƠÔƯĂÊÂĐ]{1,}))*)$", ErrorMessage = "Tên chưa đúng định dạng")]
         public string Name { get; set; }
         public string? Description { get; set; }
         public bool? Status { get; set; }
