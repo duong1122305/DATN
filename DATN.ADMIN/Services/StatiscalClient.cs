55fd2a7a95904b497cbcbf2350d4bd0f0c193df0
Nguyễn Việt <nonamesoldt@gmail.com>
2024-08-17 22:58:08 +0700

    commit

--- DATN.ADMIN/Services/StatiscalClient.cs
+++ DATN.ADMIN/Services/StatiscalClient.cs
@@ -12,18 +12,20 @@ namespace DATN.ADMIN.Services
         private readonly HttpClient _httpClient;
         private readonly IHttpContextAccessor _httpContextAccessor;
 
-		public StatiscalClient(HttpClient client)
-		{
-			_httpClient = client;
-		}
+        public StatiscalClient(HttpClient client, IHttpContextAccessor httpContextAccessor)
+        {
+            _httpClient = client;
+            _httpContextAccessor = httpContextAccessor;
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _httpContextAccessor.HttpContext.Session.GetString("Key"));
+        }
 
-		public async Task<ResponseData<BookingDataStatiscal>> GetDataBooking(int bookingID)
-		{
+        public async Task<ResponseData<BookingDataStatiscal>> GetDataBooking(int bookingID)
+        {
             return await _httpClient.GetFromJsonAsync<ResponseData<BookingDataStatiscal>>($"api/Statistical/get-data-in-bill?bookingID={bookingID}");
         }
 
-		public async Task<ResponseData<List<HistoryBookingVM>>> GetHistoryBooking(int bookingID)
-		{
+        public async Task<ResponseData<List<HistoryBookingVM>>> GetHistoryBooking(int bookingID)
+        {
             return await _httpClient.GetFromJsonAsync<ResponseData<List<HistoryBookingVM>>>($"api/Statistical/get-history-bill?bookingID={bookingID}");
         }
 
@@ -38,15 +40,6 @@ namespace DATN.ADMIN.Services
             return await _httpClient.GetFromJsonAsync<ResponseData<List<StatiscalBill>>>($"api/Statistical/get-all-bill?type={type}");
         }
 
-        public async Task<ResponseData<Statistical>> StatisticalIndex(string? startDate, string? endDate, int type = 1)
-		{
-			if (startDate != null&& endDate!=null)
-        public StatiscalClient(HttpClient client, IHttpContextAccessor httpContextAccessor)
-        {
-            _httpClient = client;
-            _httpContextAccessor = httpContextAccessor;
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _httpContextAccessor.HttpContext.Session.GetString("Key"));
-        }
         public async Task<ResponseData<Statistical>> StatisticalIndex(string? startDate, string? endDate, int type = 1)
         {
             if (startDate != null && endDate != null)
