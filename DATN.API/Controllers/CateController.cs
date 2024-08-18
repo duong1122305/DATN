7240417b2b73d8aa243344aa2723a29a4de42021
Nguyễn Việt <nonamesoldt@gmail.com>
2024-08-18 20:25:59 +0700

    push

--- DATN.API/Controllers/CateController.cs
+++ DATN.API/Controllers/CateController.cs
@@ -11,7 +11,6 @@ namespace DATN.API.Controllers
     [Route("api/[controller]")]
     [ApiController]
     [EnableCors("AllowSpecificOrigin")]
-    [Authorize(Roles = "Admin")]
     public class CateController : Controller
     {
         ICategoryManagementService _categoryManagement;
@@ -21,17 +20,23 @@ namespace DATN.API.Controllers
             _categoryManagement = categoryManagement;
             _categoryProductManagement = categoryProductManagement;
         }
+        [Authorize(Roles = "Admin")]
+
         [HttpPost("Create-Category")]
         public async Task<ResponseData<string>> CreateCategory(CategoryView categoryView)
         {
             return await _categoryManagement.CreateCategory(categoryView);
         }
+        [Authorize(Roles = "Admin")]
+
         [HttpPatch("Update-Category")]
 
         public async Task<ResponseData<string>> UpdateCategory(CategoryView categoryView)
         {
             return await _categoryManagement.UpdateCategory(categoryView);
         }
+        [Authorize(Roles = "Admin")]
+
         [HttpGet("Remove-Category")]
         public async Task<ResponseData<string>> RemoveCategory(int id)
         {
@@ -42,22 +47,30 @@ namespace DATN.API.Controllers
         {
             return await _categoryManagement.ListCategory();
         }
+        [Authorize(Roles = "Admin")]
+
         [HttpGet("Active-Category")]
         public async Task<ResponseData<string>> ActiveCategory(int id)
         {
             return await _categoryManagement.Active(id);
         }
+        [Authorize(Roles = "Admin")]
+
         [HttpPost("Create-Category-Product")]
         public Task<ResponseData<string>> CreateCategoryProduct(CreateCategoryProductView categoryView)
         {
             return _categoryProductManagement.CreateCategory(categoryView);
         }
+        [Authorize(Roles = "Admin")]
+
         [HttpPatch("Update-Category-Product")]
 
         public Task<ResponseData<string>> UpdateCategoryProduct(CreateCategoryProductView categoryView)
         {
             return _categoryProductManagement.UpdateCategory(categoryView);
         }
+        [Authorize(Roles = "Admin")]
+
         [HttpGet("Remove-Category-Product")]
         public Task<ResponseData<string>> RemoveCategoryProduct(int id)
         {
@@ -68,6 +81,8 @@ namespace DATN.API.Controllers
         {
             return await _categoryProductManagement.ListCategory();
         }
+        [Authorize(Roles = "Admin")]
+
         [HttpGet("Active-Category-Product")]
         public Task<ResponseData<string>> ActiveCategoryProduct(int id)
         {
