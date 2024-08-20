7a254f09233c205a59213b526f66338d99fb5842
Nguyễn Việt <nonamesoldt@gmail.com>
2024-08-20 08:13:06 +0700

    fix

--- DATN.Aplication/Services/BookingManagement.cs
+++ DATN.Aplication/Services/BookingManagement.cs
@@ -35,8 +35,7 @@ namespace DATN.Aplication.Services
         private readonly IProductManagement _productManagement;
         private readonly IEmployeeScheduleManagementService _employeeScheduleManagementService;
         private readonly IVoucherManagementService _voucherManagementService;
-        private readonly Utils utils;
-        public BookingManagement(IUnitOfWork unitOfWork, UserManager<User> userManager, NotificationHub notificationHub, IAuthenticate authenticate, IProductManagement productManagement, IEmployeeScheduleManagementService employeeScheduleManagementService, IVoucherManagementService voucherManagementService, Utils utils)
+        public BookingManagement(IUnitOfWork unitOfWork, UserManager<User> userManager, NotificationHub notificationHub, IAuthenticate authenticate, IProductManagement productManagement, IEmployeeScheduleManagementService employeeScheduleManagementService, IVoucherManagementService voucherManagementService)
         {
             _unitOfWork = unitOfWork;
             _userManager = userManager;
@@ -45,7 +44,6 @@ namespace DATN.Aplication.Services
             _productManagement = productManagement;
             _employeeScheduleManagementService = employeeScheduleManagementService;
             _voucherManagementService = voucherManagementService;
-            this.utils = utils;
         }
         public async Task ChangeStatusBooking()
         {
@@ -1678,65 +1676,64 @@ namespace DATN.Aplication.Services
                 return new ResponseData<string> { IsSuccess = false, Error = "Chưa chọn dịch vụ không thể đặt lịch!" };
             }
         }
-        public async Task<ResponseData<string>> PaymentQrVnPay(long totalPrice)
-        {
-            string vnp_Returnurl = "https://localhost:7259/ListServicesBooking"; //URL nhan ket qua tra ve 
-            string vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html"; //URL thanh toan cua VNPAY 
-            string vnp_TmnCode = ""; //Ma định danh merchant kết nối (Terminal Id)
-            string vnp_HashSecret = ""; //Secret Key
+        //public async Task<ResponseData<string>> PaymentQrVnPay(long totalPrice)
+        //{
+        //    string vnp_Returnurl = "https://localhost:7259/ListServicesBooking"; //URL nhan ket qua tra ve 
+        //    string vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html"; //URL thanh toan cua VNPAY 
+        //    string vnp_TmnCode = ""; //Ma định danh merchant kết nối (Terminal Id)
+        //    string vnp_HashSecret = ""; //Secret Key
 
-            //Get payment input
-            OrderInfo order = new OrderInfo();
-            order.OrderId = DateTime.Now.Ticks; // Giả lập mã giao dịch hệ thống merchant gửi sang VNPAY
-            order.Amount = totalPrice; // Giả lập số tiền thanh toán hệ thống merchant gửi sang VNPAY 100,000 VND
-            order.Status = "0"; //0: Trạng thái thanh toán "chờ thanh toán" hoặc "Pending" khởi tạo giao dịch chưa có IPN
-            order.CreatedDate = DateTime.Now;
-            //Save order to db
+        //    //Get payment input
+        //    OrderInfo order = new OrderInfo();
+        //    order.OrderId = DateTime.Now.Ticks; // Giả lập mã giao dịch hệ thống merchant gửi sang VNPAY
+        //    order.Amount = totalPrice; // Giả lập số tiền thanh toán hệ thống merchant gửi sang VNPAY 100,000 VND
+        //    order.Status = "0"; //0: Trạng thái thanh toán "chờ thanh toán" hoặc "Pending" khởi tạo giao dịch chưa có IPN
+        //    order.CreatedDate = DateTime.Now;
+        //    //Save order to db
 
-            //Build URL for VNPAY
-            VnPayLibrary vnpay = new VnPayLibrary();
+        //    //Build URL for VNPAY
+        //    VnPayLibrary vnpay = new VnPayLibrary();
 
-            vnpay.AddRequestData("vnp_Version", VnPayLibrary.VERSION);
-            vnpay.AddRequestData("vnp_Command", "pay");
-            vnpay.AddRequestData("vnp_TmnCode", vnp_TmnCode);
-            vnpay.AddRequestData("vnp_Amount", (order.Amount * 100).ToString()); //Số tiền thanh toán. Số tiền không mang các ký tự phân tách thập phân, phần nghìn, ký tự tiền tệ. Để gửi số tiền thanh toán là 100,000 VND (một trăm nghìn VNĐ) thì merchant cần nhân thêm 100 lần (khử phần thập phân), sau đó gửi sang VNPAY là: 10000000
-            vnpay.AddRequestData("vnp_BankCode", "VNPAYQR");
-            vnpay.AddRequestData("vnp_CreateDate", order.CreatedDate.ToString("yyyyMMddHHmmss"));
-            vnpay.AddRequestData("vnp_CurrCode", "VND");
-            vnpay.AddRequestData("vnp_IpAddr", utils.GetIpAddress());
-            vnpay.AddRequestData("vnp_Locale", "vn");
-            vnpay.AddRequestData("vnp_OrderInfo", "Thanh toan don hang:" + order.OrderId);
-            vnpay.AddRequestData("vnp_OrderType", "other"); //default value: other
-            vnpay.AddRequestData("vnp_ReturnUrl", vnp_Returnurl);
-            vnpay.AddRequestData("vnp_TxnRef", order.OrderId.ToString()); // Mã tham chiếu của giao dịch tại hệ thống của merchant. Mã này là duy nhất dùng để phân biệt các đơn hàng gửi sang VNPAY. Không được trùng lặp trong ngày
+        //    vnpay.AddRequestData("vnp_Version", VnPayLibrary.VERSION);
+        //    vnpay.AddRequestData("vnp_Command", "pay");
+        //    vnpay.AddRequestData("vnp_TmnCode", vnp_TmnCode);
+        //    vnpay.AddRequestData("vnp_Amount", (order.Amount * 100).ToString()); //Số tiền thanh toán. Số tiền không mang các ký tự phân tách thập phân, phần nghìn, ký tự tiền tệ. Để gửi số tiền thanh toán là 100,000 VND (một trăm nghìn VNĐ) thì merchant cần nhân thêm 100 lần (khử phần thập phân), sau đó gửi sang VNPAY là: 10000000
+        //    vnpay.AddRequestData("vnp_BankCode", "VNPAYQR");
+        //    vnpay.AddRequestData("vnp_CreateDate", order.CreatedDate.ToString("yyyyMMddHHmmss"));
+        //    vnpay.AddRequestData("vnp_CurrCode", "VND");
+        //    vnpay.AddRequestData("vnp_IpAddr", utils.GetIpAddress());
+        //    vnpay.AddRequestData("vnp_Locale", "vn");
+        //    vnpay.AddRequestData("vnp_OrderInfo", "Thanh toan don hang:" + order.OrderId);
+        //    vnpay.AddRequestData("vnp_OrderType", "other"); //default value: other
+        //    vnpay.AddRequestData("vnp_ReturnUrl", vnp_Returnurl);
+        //    vnpay.AddRequestData("vnp_TxnRef", order.OrderId.ToString()); // Mã tham chiếu của giao dịch tại hệ thống của merchant. Mã này là duy nhất dùng để phân biệt các đơn hàng gửi sang VNPAY. Không được trùng lặp trong ngày
 
-            //Add Params of 2.1.0 Version
-            //Billing
-            vnpay.AddRequestData("vnp_ExpireDate", DateTime.Now.AddMinutes(15).ToString("yyyyMMddHHmmss"));
-            //Billing
+        //    //Add Params of 2.1.0 Version
+        //    //Billing
+        //    vnpay.AddRequestData("vnp_ExpireDate", DateTime.Now.AddMinutes(15).ToString("yyyyMMddHHmmss"));
+        //    //Billing
 
-            //var fullName = "Nguyễn Đức Việt".Trim();
-            //if (!String.IsNullOrEmpty(fullName))
-            //{
-            //    var indexof = fullName.IndexOf(' ');
-            //    vnpay.AddRequestData("vnp_Bill_FirstName", fullName.Substring(0, indexof));
-            //    vnpay.AddRequestData("vnp_Bill_LastName", fullName.Substring(indexof + 1,
-            //    fullName.Length - indexof - 1));
-            //}
-            //vnpay.AddRequestData("vnp_Bill_Address", "vn".Trim());
-            //vnpay.AddRequestData("vnp_Bill_City", "".Trim());
-            //vnpay.AddRequestData("vnp_Bill_Country", "".Trim());
-            //vnpay.AddRequestData("vnp_Bill_State", "");
-            //// Invoice
-            //vnpay.AddRequestData("vnp_Inv_Phone", "".Trim());
-            //vnpay.AddRequestData("vnp_Inv_Email", "".Trim());
-            //vnpay.AddRequestData("vnp_Inv_Customer", "".Trim());
-            //vnpay.AddRequestData("vnp_Inv_Address", "".Trim());
-            //vnpay.AddRequestData("vnp_Inv_Type", "");
-            string paymentUrl = vnpay.CreateRequestUrl(vnp_Url, vnp_HashSecret);
-            return new ResponseData<string> { IsSuccess = true, Data = paymentUrl };
-        }
-
+        //    //var fullName = "Nguyễn Đức Việt".Trim();
+        //    //if (!String.IsNullOrEmpty(fullName))
+        //    //{
+        //    //    var indexof = fullName.IndexOf(' ');
+        //    //    vnpay.AddRequestData("vnp_Bill_FirstName", fullName.Substring(0, indexof));
+        //    //    vnpay.AddRequestData("vnp_Bill_LastName", fullName.Substring(indexof + 1,
+        //    //    fullName.Length - indexof - 1));
+        //    //}
+        //    //vnpay.AddRequestData("vnp_Bill_Address", "vn".Trim());
+        //    //vnpay.AddRequestData("vnp_Bill_City", "".Trim());
+        //    //vnpay.AddRequestData("vnp_Bill_Country", "".Trim());
+        //    //vnpay.AddRequestData("vnp_Bill_State", "");
+        //    //// Invoice
+        //    //vnpay.AddRequestData("vnp_Inv_Phone", "".Trim());
+        //    //vnpay.AddRequestData("vnp_Inv_Email", "".Trim());
+        //    //vnpay.AddRequestData("vnp_Inv_Customer", "".Trim());
+        //    //vnpay.AddRequestData("vnp_Inv_Address", "".Trim());
+        //    //vnpay.AddRequestData("vnp_Inv_Type", "");
+        //    string paymentUrl = vnpay.CreateRequestUrl(vnp_Url, vnp_HashSecret);
+        //    return new ResponseData<string> { IsSuccess = true, Data = paymentUrl };
+        //}
         public async Task<ResponseData<List<GetBookingByGuestVM>>> GetBookingByGuest(Guid idGuest)
         {
             try
