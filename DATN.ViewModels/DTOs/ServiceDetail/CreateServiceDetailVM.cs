9b41e57cf94d3977b0b3dba8f7df5f0f9270204c
HUNGNV <hungwnv2001@gmail.com>
2024-08-25 14:16:19 +0700

    fix bỏ cân nặng

--- DATN.ViewModels/DTOs/ServiceDetail/CreateServiceDetailVM.cs
+++ DATN.ViewModels/DTOs/ServiceDetail/CreateServiceDetailVM.cs
@@ -12,23 +12,14 @@ namespace DATN.ViewModels.DTOs.ServiceDetail
         //[MinLength(5, ErrorMessage = "Tối thiểu 5 ký tự")]
         //public string Name { get; set; } // Tên chi tiết dịch vụ
 
-        [Range(1000, 9999999, ErrorMessage = "Giá không được nhỏ hơn 100 hoặc lớn hơn 9999999")]
+        [Range(1000, 10000000, ErrorMessage = "Giá phải từ 1.000 đến 10,000,000 vnđ")]
         public float Price { get; set; } // Giá tiền
 
-        [Range(1, 9999, ErrorMessage = "Thời gian phải lớn hơn 1 và ít hơn 9999")]
+        [Range(1, 90, ErrorMessage = "Thời gian phải từ 1-90 phút")]
         public double Duration { get; set; } // Khoảng thời gian
 
-        [Required(ErrorMessage = "Mô tả không được để trống")]
+        [Required(ErrorMessage = "Tên dịch vụ chi tiết không được để trống")]
         public string Description { get; set; } // Mô tả
-
-        [Required(ErrorMessage = "Bắt buộc nhập cân nặng tối thiểu")]
-        [Range(1, 100, ErrorMessage = "Cân nặng không được nhỏ hơn 1 hoặc lớn hơn 100")]
-        public float MinWeight { get; set; }
-
-        [Required(ErrorMessage = "Bắt buộc nhập cân nặng tối đa")]
-        [Range(1, 100, ErrorMessage = "Cân nặng không được nhỏ hơn 1 hoặc lớn hơn 100")]
-        public float MaxWeight { get; set; }
-
         public bool IsDeleted { get; set; } = false; // Mô tả
 
         public DateTime CreateAt { get; set; } // Thời gian tạo
